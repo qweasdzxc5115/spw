042e73a5a2aeb72b3692d2172c18dd7470af11e4
qweasdzxc5115 <ext_spirit@hotmail.com>
2015-04-20 00:41:31 +0700

    *06* Plus Enemy2

--- f2/spw/GameEngine.java
+++ f2/spw/GameEngine.java
@@ -14,7 +14,8 @@ import javax.swing.Timer;
 public class GameEngine implements KeyListener, GameReporter{
 	GamePanel gp;
 		
-	private ArrayList<Enemy> enemies = new ArrayList<Enemy>();	
+	private ArrayList<Enemy> enemies = new ArrayList<Enemy>();
+	private ArrayList<Enemy2> enemies2 = new ArrayList<Enemy2>();
 	private SpaceShip v;	
 	
 	private Timer timer;
@@ -52,6 +53,12 @@ public class GameEngine implements KeyListener, GameReporter{
 		enemies.add(e);
 	}
 	
+	private void generateEnemy2(){
+		Enemy2 e = new Enemy2((int)(Math.random()*390), 30);
+		gp.sprites.add(e);
+		enemies2.add(e);
+	}
+	
 	private void process(){
 		if(time>0)
 			time--;
@@ -74,6 +81,25 @@ public class GameEngine implements KeyListener, GameReporter{
 			}
 		}
 		
+	private void process2(){
+		if(Math.random()<difficulty){
+			generateEnemy2();
+		}
+		
+		Iterator<Enemy2> e_iter = enemies2.iterator();
+		while(e_iter.hasNext()){
+			Enemy2 e = e_iter.next();
+			e.proceed();
+			
+			if(!e.isAlive()){
+				e_iter.remove();
+				gp.sprites.remove(e);
+				score += 300;
+			}
+	}	
+		
+		
+		
 		gp.updateGameUI(this);
 		
 		Rectangle2D.Double vr = v.getRectangle();
@@ -92,6 +118,10 @@ public class GameEngine implements KeyListener, GameReporter{
 			}
 		}
 	}
+	
+	
+	
+	
 	gp.HP(x,y);
 	}
 	public void die(){
