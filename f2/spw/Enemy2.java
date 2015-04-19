2b8b40837da3893ff53245b4335f1ae9eb8a2d9c
qweasdzxc5115 <ext_spirit@hotmail.com>
2015-04-20 00:30:46 +0700

    *06* Plus Enemy2

--- f2/spw/Enemy2.java
+++ f2/spw/Enemy2.java
@@ -6,7 +6,7 @@ public class Enemy2 extends Sprite{
 	public static final int Y_TO_FADE = 400;
 	public static final int Y_TO_DIE = 600;
 	
-	private int step = 15;
+	private int step = 14;
 	private boolean alive = true;
 	
 	public Enemy(int x, int y) {
