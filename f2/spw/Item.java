2ce94a0aa3e601063cd79cae247045e660a0b70c
qweasdzxc5115 <ext_spirit@hotmail.com>
2015-04-29 22:18:54 +0700

    *09* item

---
+++ f2/spw/Item.java
@@ -0,0 +1,42 @@
+package f2.spw;
+
+import java.awt.AlphaComposite;
+import java.awt.Color;
+import java.awt.Graphics2D;
+
+import java.awt.Toolkit;
+import java.awt.Image;
+
+public class item  extends Enemy{
+  private int  step = 5;
+  private boolean alive = true;
+  
+  public item(int x, int y){
+    super(x,y);
+  }
+  
+  @Override
+  public void draw(Graphics2D g) {
+		if(y < Y_TO_FADE)
+			g.setComposite(AlphaComposite.getInstance(AlphaComposite.SRC_OVER, 1.0f));
+		else{
+			g.setComposite(AlphaComposite.getInstance(AlphaComposite.SRC_OVER, 
+					(float)(Y_TO_DIE - y)/(Y_TO_DIE - Y_TO_FADE)));
+		}
+		Image img = Toolkit.getDefaultToolkit().getImage("hp.GIF");
+		g.drawImage(img, x, y,42,42,null); 
+	}
+	
+		public void proceed(){
+		y += step;
+		if(y > Y_TO_DIE){
+			alive = false;
+		}
+	}
+	public boolean isAlive(){
+		return alive;
+	}
+	public void getIntersect(){
+		this.alive = false;
+	}
+}
